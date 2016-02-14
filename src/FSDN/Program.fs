bd65d49b1efc8294394448fa3e8d87b86f48d2cc
pocketberserker <pocketberserker@gmail.com>
2016-02-14 09:16:26 +0900

    get config from arguments

--- src/FSDN/Program.fs
+++ src/FSDN/Program.fs
@@ -1,4 +1,4 @@
-﻿
+﻿open System.IO
 open System.Net
 open Suave
 open Suave.Web
@@ -6,22 +6,50 @@ open Suave.Logging
 open Suave.Operators
 open Suave.Filters
 open Suave.Files
+open Argu
 
-// TODO: get from config
-let logger = Loggers.ConsoleWindowLogger LogLevel.Verbose
+type Args =
+  | Port of Sockets.Port
+  | Home_Directory of string
+  | Log_Level of string
+with
+  interface IArgParserTemplate with
+    member this.Usage =
+      match this with
+      | Port _ -> "specify a primary port."
+      | Home_Directory _ -> "specify a home or root diretory."
+      | Log_Level _ -> "specify log level."
 
-let app : WebPart =
-  choose [
-    log logger logFormat >=> never
-    GET >=> path "/" >=> file "index.html"
-  ]
+let configAndApp (args: ParseResults<Args>) : (SuaveConfig * WebPart) =
 
-let serverConfig =
-  // TODO: get from config
-  let port = "80" |> Sockets.Port.Parse
-  { defaultConfig with bindings = [ HttpBinding.mk HTTP IPAddress.Loopback port ] }
+  let home = DirectoryInfo(args.GetResult(<@ Home_Directory @>, ".")).FullName
+  let logger =
+    match args.TryPostProcessResult(<@ Log_Level @>, LogLevel.FromString) with
+    | Some l -> l
+    | None -> LogLevel.Warn
+    |> Loggers.ConsoleWindowLogger
+  
+  let app =
+    choose [
+      log logger logFormat >=> never
+      GET >=> path "/" >=> browseFile home "index.html"
+    ]
+
+  let serverConfig = {
+    defaultConfig with
+      bindings = [ HttpBinding.mk HTTP IPAddress.Loopback (args.GetResult(<@ Port @>, 8083us)) ]
+      homeFolder =
+        args.TryGetResult(<@ Home_Directory @>)
+        |> Option.map (fun d -> DirectoryInfo(d).FullName)
+      logger = logger
+  }
+
+  (serverConfig, app)
+
+let parser = ArgumentParser.Create<Args>()
 
 [<EntryPoint>]
-let main argv = 
-  startWebServer serverConfig app
+let main args =
+  let args = parser.Parse(args)
+  startWebServer <|| configAndApp args
   0
